4892456bdc79cebbc85c56a0b9de6fc4ae4fecf9
J3rry-L <jliang21@stuy.edu>
2021-01-11 17:38:39 -0500

    Did prelim testing for radixSortSimple method

--- Tester.java
+++ Tester.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Tester{
   public static void main(String[] args){
     System.out.println(Radix.nth(123,1));
@@ -8,5 +9,16 @@ public class Tester{
     System.out.println(Radix.length(15));
     System.out.println(Radix.length(-10));
     System.out.println(Radix.length(5112));
+    SortableLinkedList list1 = new SortableLinkedList();
+    Random rngSeed = new Random();
+    int seed = rngSeed.nextInt();
+    Random rng = new Random(seed);
+    System.out.println(seed);
+    for (int i = 0; i < 10; i++){
+      list1.add(Math.abs(rng.nextInt() % 1000));
+    }
+    System.out.println(list1);
+    Radix.radixSortSimple(list1);
+    System.out.println(list1);
   }
 }
