2f39b7f70110d0bcda180087636c96394c3603fa
J3rry-L <jliang21@stuy.edu>
2021-01-11 16:02:36 -0500

    Fixed typo

--- Radix.java
+++ Radix.java
@@ -11,7 +11,7 @@ public class Radix{
     }
   }
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
-    for (int i = 0; i < buckets.length(); i++){
+    for (int i = 0; i < buckets.length; i++){
       original.extend(buckets[i]);
     }
   }
