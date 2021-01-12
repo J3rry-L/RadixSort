91c374573dbecc59cea224179cb83056cc206ccd
J3rry-L <jliang21@stuy.edu>
2021-01-11 19:59:34 -0500

    Created radixSort method for any integer

--- Radix.java
+++ Radix.java
@@ -39,4 +39,44 @@ public class Radix{
       merge(data, buckets);
     }
   }
+  public static void radixSort(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    SortableLinkedList[] signs = new SortableLinkedList[2];
+    int maxLength = 0;
+    for (int i = 0; i < 10; i++){
+      buckets[i] = new SortableLinkedList();
+    }
+    for (int i = 0; i < 2; i++){
+      signs[i] = new SortableLinkedList();
+    }
+    while(data.size() > 0){
+      if(length(data.get(0)) > maxLength){
+        maxLength = length(data.get(0));
+      }
+      buckets[nth(data.get(0), 0)].add(data.get(0));
+      //System.out.println(buckets[nth(data.get(0), 0)]);
+      data.remove(0);
+    }
+    merge(data, buckets);
+    for (int i = 1; i < maxLength; i++){
+      while(data.size() > 0){
+        buckets[nth(data.get(0), i)].add(data.get(0));
+        //System.out.println(buckets[nth(data.get(0), i)]);
+        data.remove(0);
+      }
+      merge(data, buckets);
+    }
+    while(data.size() > 0){
+      if(data.get(0) > 0){
+        signs[1].add(data.get(0));
+        //System.out.println(signs[1]);
+      }
+      else{
+        signs[0].add(0, data.get(0));
+        //System.out.println(signs[0]);
+      }
+      data.remove(0);
+    }
+    merge(data, signs);
+  }
 }
