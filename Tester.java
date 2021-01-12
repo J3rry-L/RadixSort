37334c11845955875b22122cf30338126c74d90f
J3rry-L <jliang21@stuy.edu>
2021-01-11 19:59:04 -0500

    Tested output and time

--- Tester.java
+++ Tester.java
@@ -25,6 +25,17 @@ public class Tester{
     Radix.radixSortSimple(test1);
     System.out.println(test1);
 
+    SortableLinkedList test2 = new SortableLinkedList();
+    test2.add(100000);
+    test2.add(-10000);
+    test2.add(1000);
+    test2.add(-100);
+    test2.add(10);
+    test2.add(-1);
+    test2.add(0);
+    Radix.radixSort(test2);
+    System.out.println(test2);
+
     SortableLinkedList list1 = new SortableLinkedList();
     Random rngSeed = new Random();
     int seed = rngSeed.nextInt();
@@ -106,8 +117,87 @@ public class Tester{
     System.out.println((finish - start) / 1000000.0);
     //System.out.println(list7);
 
+
+    SortableLinkedList list8 = new SortableLinkedList();
+    for (int i = 0; i < 10; i++){
+      list8.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list8);
+    start = System.nanoTime();
+    Radix.radixSort(list8);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list8);
+
+    SortableLinkedList list9 = new SortableLinkedList();
+    for (int i = 0; i < 100; i++){
+      list9.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list9);
+    start = System.nanoTime();
+    Radix.radixSort(list9);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list9);
+
+    SortableLinkedList list10 = new SortableLinkedList();
+    for (int i = 0; i < 1000; i++){
+      list10.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list10);
+    start = System.nanoTime();
+    Radix.radixSort(list10);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list10);
+
+    SortableLinkedList list11 = new SortableLinkedList();
+    for (int i = 0; i < 10000; i++){
+      list11.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list11);
+    start = System.nanoTime();
+    Radix.radixSort(list11);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list11);
+
+    SortableLinkedList list12 = new SortableLinkedList();
+    for (int i = 0; i < 100000; i++){
+      list12.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list12);
+    start = System.nanoTime();
+    Radix.radixSort(list12);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list12);
+
+    SortableLinkedList list13 = new SortableLinkedList();
+    for (int i = 0; i < 1000000; i++){
+      list13.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list13);
+    start = System.nanoTime();
+    Radix.radixSort(list13);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list13);
+
+    SortableLinkedList list14 = new SortableLinkedList();
+    for (int i = 0; i < 10000000; i++){
+      list14.add(rng.nextInt() % 1000);
+    }
+    //System.out.println(list14);
+    start = System.nanoTime();
+    Radix.radixSort(list14);
+    finish = System.nanoTime();
+    System.out.println((finish - start) / 1000000.0);
+    //System.out.println(list14);
+
     System.out.println(list1);
     System.out.println(list2);
-
+    System.out.println(list8);
+    System.out.println(list9);
   }
 }
